5efb6dd0360bbc95a94b9f77310505f6da1952c7
Soham Sagade <Soham.Sagade@stratasys.com>
2023-11-03 10:15:56 +0530

    feature(Iot): integrate Iot mechanism to pilot backend

--- src/graphql/Job/job.resolver.ts
+++ src/graphql/Job/job.resolver.ts
@@ -1,3 +1,4 @@
+import { IotApi } from "../../Iot/iotAPIs";
 import { JobDao } from "./job.dao";
 import { Job } from "./job.model";
 
@@ -19,6 +20,15 @@ export const jobMutations = {
     try {
       const jobData = args.jobData;
       const dao = new JobDao();
+      const iot = new IotApi();
+      if (
+        !iot.updateIotDeviceStatus(
+          args.jobData.deviceId,
+          args.jobData.status
+        ) &&
+        true
+      )
+        return;
       const createdJob: Job = await dao.createJob(jobData);
       return createdJob;
     } catch (error) {
@@ -29,6 +39,15 @@ export const jobMutations = {
   updateJobStatus: async (parent, args, context, info) => {
     try {
       const jobData = args.actionObject;
+      const iot = new IotApi();
+      if (
+        !iot.updateIotDeviceStatus(
+          args.actionObject.deviceId,
+          args.actionObject.status
+        ) &&
+        true 
+      )
+        return;
       const dao = new JobDao();
       const updatedJob: Job = await dao.updateJobStatus(jobData);
       return updatedJob;
