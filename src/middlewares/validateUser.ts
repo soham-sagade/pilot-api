159465654b069c72c1f63831f5d4e1bded759cc2
Soham Sagade <Soham.Sagade@stratasys.com>
2023-11-07 18:26:38 +0530

    chore: enable authToken validation middleware

--- src/middlewares/validateUser.ts
+++ src/middlewares/validateUser.ts
@@ -3,16 +3,15 @@ import { AppDataSource } from "../../dbConnection";
 import { Userdata } from "../models/userModel";
 export default async function validateUser(req, res, next) {
   try {
-    //TODO: Commented to inspect graphQL dashboard, will be enabled later
-    // const {authToken} = req.headers;
-    // const data: any = jwt.verify(
-    //   authToken,
-    //   process.env.SECRETKEY
-    // );
-    // const user = await AppDataSource.manager.findOneBy(Userdata, {
-    //   userName: data.username,
-    // });
-    // if(!user) return res.status(404).send("User not found!");
+    const {authToken} = req.headers;
+    const data: any = jwt.verify(
+      authToken,
+      process.env.SECRETKEY
+    );
+    const user = await AppDataSource.manager.findOneBy(Userdata, {
+      user_name: data.username,
+    });
+    if(!user) return res.status(404).send("User not found!");
     next();
   } catch (error) {
     return res.status(500).send("Something went wrong!");
